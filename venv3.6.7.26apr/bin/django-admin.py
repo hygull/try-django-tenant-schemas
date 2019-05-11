8270d4bea3817630a8f048ab4c104ee95c03983b
Rishikesh Agrawani <rishikesh0014051992@gmail.com>
2019-05-11 19:27:31 +0530

    updated .gitignore

--- venv3.6.7.26apr/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/hygull/Desktop/try_django_tenant_schemas/venv3.6.7.26apr/bin/python3.6
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
